dcd927d731394c9e5a79332fa8376e11ccdc886e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-01 21:29:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,20 @@ function distanceTravelledInFeet(startBlock, endBlock) {
 }
 
 function calculatesFarePrice(startBlock, endBlock) {
-  
+  let distanceinFeet = Math.abs(startBlock - endBlock)*264;
+  let price;
+  switch (distanceinFeet) {
+    case distanceinFeet <= 400:
+      price = 0;
+      return price;
+      case distanceinFeet > 400 && distanceinFeet <= 2000:
+      price = distanceinFeet * .02;
+      return price;
+      case distanceinFeet > 2000 && <= 2500:
+      price = 25
+      return price;
+      default:
+      price = "cannot travel that far"
+      return price
+  }
 }
